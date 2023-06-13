262a845b8b88cc0a893574499bdc714860d98309
Arnold Atsé <arnold.arnold@gmail.com>
2023-06-13 17:44:42 +0000

    fix(signIn-test): fixed signIn test

--- src/app/auth/sign-in/sign-in.component.spec.ts
+++ src/app/auth/sign-in/sign-in.component.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { SignInService } from '../services/sign-in/sign-in.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -45,33 +48,63 @@ describe('SignInComponent', () => {
       fixture.whenStable().then(() => {
         const loginError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-login-error")
         const passwordError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-password-error")
+        const signInSubmitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector("#sign-in-submit-button")
 
         expect(loginError.innerHTML.trim()).toEqual('Login is required')
         expect(passwordError.innerHTML.trim()).toEqual('Password is required')
+        expect(signInSubmitButton.disabled).toBeTruthy()
       })
     })
   })
 
-  /* it('form submitable', async () => {
-    fixture.whenStable().then(async () => {
-      const loginElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-login')
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-password')
+  it('form submitable', async () => {
+    const loginElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-login')
+    const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-password')
 
-      console.log("loginElement 1: ", loginElement)
+    loginElement.value = "login"
+    passwordElement.value = "password"
 
-      loginElement.value = "login"
-      passwordElement.value = "password"
+    loginElement.dispatchEvent(new Event("input"))
+    passwordElement.dispatchEvent(new Event("input"))
 
-      fixture.detectChanges()
-      fixture.whenStable().then(() => {
-        const loginError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-login-error")
-        const passwordError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-password-error")
-        const submitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#sign-in-submit-button')
+    fixture.detectChanges()
+    await fixture.whenStable().then(() => {
+      const loginError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-login-error")
+      const passwordError: HTMLDivElement = fixture.debugElement.nativeElement.querySelector("#sign-in-password-error")
+      const submitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#sign-in-submit-button')
 
-        expect(loginError.hidden).toBeTruthy()
-        expect(passwordError.hidden).toBeTruthy()
-        expect(submitButton.disabled).toBeFalsy()
-      })
+      expect(loginError).toBeFalsy()
+      expect(passwordError).toBeFalsy()
+      expect(submitButton.disabled).toBeFalsy()
     })
-  }) */
+  })
+
+
+  it('should signin with good params and navigate to space on submit', async ()=>{
+    const loginElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-login')
+    const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#sign-in-password')
+
+    loginElement.value = "login"
+    passwordElement.value = "password"
+
+    loginElement.dispatchEvent(new Event("input"))
+    passwordElement.dispatchEvent(new Event("input"))
+
+    fixture.detectChanges()
+    await fixture.whenStable().then(()=>{
+      const submitButtonElement: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#sign-in-submit-button')
+
+      const signInService = fixture.debugElement.injector.get(SignInService)
+      const routerService = fixture.debugElement.injector.get(Router)
+      const spySignInServiceExecute = spyOn(signInService, "execute").and.returnValue(
+        of({ echec: false, message: "created with success", user: { token: "SDdFCcSDQCeeeNECEE6fz46efze6f4e84f8zzeizflnSDF" } })
+      )
+      const spyRouterServiceNavigate = spyOn(routerService, "navigate").and.returnValue(Promise.resolve(true))
+
+      submitButtonElement.click()
+      expect(spySignInServiceExecute).toHaveBeenCalledWith("login", "password", false)
+      expect(spyRouterServiceNavigate).toHaveBeenCalledWith(["space"])
+    })
+
+  })
 });
